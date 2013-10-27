3835f5a00b40d46fdf97364ffaca36c37d028de8
Abdu Hussein <abduhussein@gmail.com>
2013-10-27 16:25:58 +0000

    FibqPanel setter changed

--- MELT/src/melt/View/BlanksMatcher.java
+++ MELT/src/melt/View/BlanksMatcher.java
@@ -4,30 +4,91 @@
  */
 package melt.View;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author mbaxjah5
  */
 public class BlanksMatcher {
 
+    private String text;
+    private char delimiter;
+    private final char escapeCahr = '\\';
+    private String regExpression;
+    private String[] matches;
+    private String[] matchesWithEscape;
+    private Pattern pattern;
+    private Matcher matcher;
+    private int numOfDelimiters;
+
+    
+    
+    public BlanksMatcher(String text, char delimiter) {
+        this.text = text.trim();
+        this.delimiter = delimiter;
+        this.regExpression = "([^\\\\" + this.delimiter + "]|\\\\.)+";
+        pattern = Pattern.compile(regExpression);
+        matcher = pattern.matcher(this.text);
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDelimiter(char delimiter) {
+        this.delimiter = delimiter;
+    }
     
     
 
-    public static void main(String[] args) {
-        String pattern = "[^\\\\]_";
-        String pattern2 = "[\\\\]_";
-        String questionText = "\\_this is _ a text _";
-        String[] blanks = questionText.split(pattern);
-        int noOfBlanks = blanks.length - 1;
-        
-        System.out.println(noOfBlanks);
-        
-        for(String str: blanks) {
-            System.out.println(str.replaceAll(pattern2,"_"));
+    public String[] getMatchesWithEscape() {
+        ArrayList<String> matchedWithEscape = new ArrayList();
+        ArrayList<String> matchedWithoutEscape = new ArrayList();
+        while (matcher.find()) {
+            matchedWithEscape.add(matcher.group());
+            matchedWithoutEscape.add(matcher.group().replaceAll("\\_", "_"));
         }
-        
-        //String text = questionText.replaceAll(pattern2,"_");
-        //System.out.println(text);
+        matchesWithEscape = matchedWithEscape.toArray(new String[matchedWithEscape.size()]);
+        matches = matchedWithoutEscape.toArray(new String[matchedWithoutEscape.size()]);
+        return matchesWithEscape;
     }
     
+    public String[] getMatches() {
+        String[] matchesWithEscape = getMatchesWithEscape();
+        for(String str: matchesWithEscape) {
+            //String strWithoutEscape = str.
+        }
+        return matches;
+    }
+
+    public int getNumOfDelimiters() {
+        String[] delimiterString = pattern.split(text);
+        numOfDelimiters = delimiterString.length;
+        char firstChar = this.text.charAt(0);
+        if (!"_".equals(String.valueOf(firstChar)) && numOfDelimiters !=0) {
+            numOfDelimiters--;
+        } 
+        
+        return numOfDelimiters;
+    }
+    
+
+    public static void main(String[] args) {
+        BlanksMatcher b = new BlanksMatcher("_This is a text\\_", '_');
+        String[] matches = b.getMatches();
+        System.out.println(matches.length);
+        
+        for (String str : matches) {
+            System.out.println(str);
+        }
+        System.out.println(b.getNumOfDelimiters());
+        
+        /*Pattern p = Pattern.compile("([^\\\\_]|\\\\.)+");
+         
+         //String text = questionText.replaceAll(pattern2,"_");
+         //System.out.println(text);*/
+    }
 }
